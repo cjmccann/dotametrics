73b233ce502ad2cff7aaeacf1293ef51db86e158
cjmccann <connor.j.mccann@gmail.com>
2015-02-02 23:57:01 -0500

    whoooo adding editing to players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,21 +1,45 @@
 class PlayersController < ApplicationController
-	def new
-	end
-
-	def create
-	  #render plain: params[:player].inspect
-	  @player = Player.new(player_params)
-
-	  @player.save
-	  redirect_to @player
+  def index
+    @players = Player.all
   end
 
   def show
     @player = Player.find(params[:id])
   end
 
-  def index
-    @players = Player.all
+	def new
+	  @player = Player.new
+	end
+
+	def edit
+	  @player = Player.find(params[:id])
+  end
+
+	def create
+	  #render plain: params[:player].inspect
+	  @player = Player.new(player_params)
+
+    if @player.save
+      redirect_to @player
+    else
+      render 'new'
+    end
+
+	  #puts @player.save
+	  #redirect_to @player
+  end
+
+  def update
+    @player = Player.find(params[:id])
+
+    if @player.update(player_params)
+      redirect_to @player
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
   end
 
   private
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ActiveRecord::Base
+  validates :username, presence: true, length: { minimum: 2 }
+  
   has_many :heroes
   has_many :matches
   has_one  :stats
